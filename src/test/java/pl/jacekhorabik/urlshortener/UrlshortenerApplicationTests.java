3853167cbbfa85574c3ab4ccd129baa3d4f9a76c
Jacek Horabik <jacek.horabik@ext.airbnb.com>
2025-01-05 18:04:56 +0100

    initial commit

---
+++ src/test/java/pl/jacekhorabik/urlshortener/UrlshortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jacekhorabik.urlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlshortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
