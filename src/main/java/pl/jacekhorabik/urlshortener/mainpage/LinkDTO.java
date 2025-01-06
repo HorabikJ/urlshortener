ff69d4332c6f1d638b36a1f12f040a98b9af4981
Jacek Horabik <jacek.horabik@ext.airbnb.com>
2025-01-06 12:45:35 +0100

    Worhing html <form> for sending the plain url to MainPageController.java.

---
+++ src/main/java/pl/jacekhorabik/urlshortener/mainpage/LinkDTO.java
@@ -0,0 +1,9 @@
+package pl.jacekhorabik.urlshortener.mainpage;
+
+record LinkDTO(String url) {
+
+    public LinkDTO() {
+        this(null);
+    }
+    
+}
--- src/main/java/pl/jacekhorabik/urlshortener/mainpage/MainPageController.java
+++ src/main/java/pl/jacekhorabik/urlshortener/mainpage/MainPageController.java
@@ -1,15 +1,24 @@
 package pl.jacekhorabik.urlshortener.mainpage;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/v1")
-public class MainPageController {
+class MainPageController {
     
     @GetMapping("/")
-    public String mainPage() {
+    public String mainPage(Model model) {
+        LinkDTO linkDTO = new LinkDTO();
+        model.addAttribute("linkDTO", linkDTO);
+        return "mainpage";
+    }
+    
+    @PostMapping("/links")
+    public String shortenLink(LinkDTO linkDTO) {
         return "mainpage";
     }
 
