3eafafa08a41845dc6b95bd75d886cc357b8670f
Jacek Horabik <jacek.horabik@ext.airbnb.com>
2025-01-08 18:27:57 +0100

    Add `createdAt` column in `urls` table.

--- src/main/java/pl/jacekhorabik/urlshortener/mainpage/UrlEntity.java
+++ src/main/java/pl/jacekhorabik/urlshortener/mainpage/UrlEntity.java
@@ -1,5 +1,6 @@
 package pl.jacekhorabik.urlshortener.mainpage;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
@@ -7,17 +8,30 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.Instant;
 
 @Entity
 @Table(name = "urls")
 @Getter
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 class UrlEntity {
+
+  public UrlEntity(String hash, String url) {
+    this.hash = hash;
+    this.url = url;
+  }
 
   @Id
   // todo add hibernate validation
   private String hash;
 
+  @Column(updatable = false, nullable = false)
   private String url;
+  
+  @CreationTimestamp
+  @Column(updatable = false, nullable = false)
+  private Instant createdAt;
+  
 }
