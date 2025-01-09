681e1e97f632fa423c973c60f3d34a6021cfea36
Jacek Horabik <jacek.horabik@ext.airbnb.com>
2025-01-09 12:44:30 +0100

    Add `updatedAt` field in UrlEntity.

--- src/main/java/pl/jacekhorabik/urlshortener/mainpage/UrlEntity.java
+++ src/main/java/pl/jacekhorabik/urlshortener/mainpage/UrlEntity.java
@@ -4,13 +4,12 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.time.Instant;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
-
-import java.time.Instant;
+import org.hibernate.annotations.UpdateTimestamp;
 
 @Entity
 @Table(name = "urls")
@@ -33,5 +32,8 @@ class UrlEntity {
   @CreationTimestamp
   @Column(updatable = false, nullable = false)
   private Instant createdAt;
-  
+
+  @UpdateTimestamp
+  @Column(nullable = false)
+  private Instant updatedAt;
 }
