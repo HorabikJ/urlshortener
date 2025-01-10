b6b91281635c10eb48d8f27c8e40e3606e881e9d
Jacek Horabik <jacek.horabik@ext.airbnb.com>
2025-01-10 13:57:44 +0100

    Implement UrlRepositoryTest.

--- src/test/java/pl/jacekhorabik/urlshortener/mainpage/UrlRepositoryTest.java
+++ src/test/java/pl/jacekhorabik/urlshortener/mainpage/UrlRepositoryTest.java
@@ -2,9 +2,6 @@ package pl.jacekhorabik.urlshortener.mainpage;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.time.Instant;
-import java.util.concurrent.TimeUnit;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -25,24 +22,8 @@ class UrlRepositoryTest {
     assertThat(urlEntity).isNotNull();
     assertThat(urlEntity.getHash()).isEqualTo("hash");
     assertThat(urlEntity.getUrl()).isEqualTo("https://example.com");
-    assertThat(urlEntity.getCreatedAt()).isBefore(Instant.now());
-    assertThat(urlEntity.getUpdatedAt()).isBefore(Instant.now());
+    assertThat(urlEntity.getCreatedAt()).isNotNull();
+    assertThat(urlEntity.getCreatedAt()).isNotNull();
+    assertThat(urlEntity.getCreatedAt()).isEqualTo(urlEntity.getUpdatedAt());
   }
-
-  @Test
-  @Disabled
-  // todo investigate why createdAt is null
-  void shouldUpdateUrlEntityUpdatedAtColumn() throws InterruptedException {
-    urlRepository.saveAndFlush(new UrlEntity("hash", "https://example.com"));
-    UrlEntity saved = urlRepository.findUrlEntityByHash("hash").get();
-
-    TimeUnit.SECONDS.sleep(1);
-
-    urlRepository.saveAndFlush(new UrlEntity("hash", "https://example.com"));
-    UrlEntity updated = urlRepository.findUrlEntityByHash("hash").get();
-
-    assertThat(saved.getCreatedAt()).isEqualTo(updated.getCreatedAt());
-    assertThat(saved.getUpdatedAt()).isBefore(updated.getUpdatedAt());
-  }
-  
 }
