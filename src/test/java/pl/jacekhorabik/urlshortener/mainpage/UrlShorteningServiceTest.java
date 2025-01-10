5bf5a6b2c512553f2a27b58eb6450a4191d8f46e
Jacek Horabik <jacek.horabik@ext.airbnb.com>
2025-01-10 10:24:54 +0100

    Delete redundant method.

--- src/test/java/pl/jacekhorabik/urlshortener/mainpage/UrlShorteningServiceTest.java
+++ src/test/java/pl/jacekhorabik/urlshortener/mainpage/UrlShorteningServiceTest.java
@@ -1,12 +1,8 @@
 package pl.jacekhorabik.urlshortener.mainpage;
 
-import static org.assertj.core.api.Assertions.assertThat;
 
 import io.seruco.encoding.base62.Base62;
-import java.time.Instant;
-import org.apache.commons.codec.DecoderException;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -24,17 +20,4 @@ class UrlShorteningServiceTest {
     urlShorteningService = new UrlShorteningService(Base62.createInstance(), urlRepository);
   }
 
-  @Test
-  // todo rename method
-  void test() throws DecoderException {
-    String hash = urlShorteningService.shortenUrl("https://www.google.pl/");
-    urlRepository.flush();
-
-    UrlEntity urlEntity = urlRepository.findUrlEntityByHash(hash).get();
-    assertThat(urlEntity).isNotNull();
-    assertThat(urlEntity.getHash()).isEqualTo(hash);
-    assertThat(urlEntity.getUrl()).isEqualTo("https://www.google.pl/");
-    assertThat(urlEntity.getCreatedAt()).isBefore(Instant.now());
-    assertThat(urlEntity.getUpdatedAt()).isBefore(Instant.now());
-  }
 }
