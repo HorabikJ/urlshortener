100e575c51726d873c63d2016f49041a1bffb7fe
Jacek Horabik <jacek.horabik@ext.airbnb.com>
2025-01-13 16:19:13 +0100

    Rename `app` to `backend` naming in k8s resources.

--- k8s-run.sh
+++ k8s-run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-docker build -t urlshortener-app:latest . 
+docker build -t urlshortener-backend:latest . 
 docker build -t urlshortener-db:latest ./mysql/ 
 
 kubectl create -f ./k8s/urlshortener-namespace.yaml
 kubectl create -f ./k8s/urlshortener-db-deployment.yaml
 kubectl create -f ./k8s/urlshortener-db-service.yaml
-kubectl create -f ./k8s/urlshortener-app-deployment.yaml
-kubectl create -f ./k8s/urlshortener-app-service.yaml
+kubectl create -f ./k8s/urlshortener-backend-deployment.yaml
+kubectl create -f ./k8s/urlshortener-backend-service.yaml
