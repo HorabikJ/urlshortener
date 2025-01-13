5ec0c1b76262a1e1943a2a17e6c9497e9eb3c9f3
Jacek Horabik <jacek.horabik@ext.airbnb.com>
2025-01-13 16:13:17 +0100

    Delete pod resource files and implement deployments.

--- k8s-run.sh
+++ k8s-run.sh
@@ -4,7 +4,7 @@ docker build -t urlshortener-app:latest .
 docker build -t urlshortener-db:latest ./mysql/ 
 
 kubectl create -f ./k8s/urlshortener-namespace.yaml
-kubectl create -f ./k8s/urlshortener-db-pod.yaml
+kubectl create -f ./k8s/urlshortener-db-deployment.yaml
 kubectl create -f ./k8s/urlshortener-db-service.yaml
-kubectl create -f ./k8s/urlshortener-app-pod.yaml
+kubectl create -f ./k8s/urlshortener-app-deployment.yaml
 kubectl create -f ./k8s/urlshortener-app-service.yaml
