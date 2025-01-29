72155ad1e80e75bb223b63197311ca50f6aaee2c
Jacek Horabik <jacek.horabik@ext.airbnb.com>
2025-01-29 16:06:42 +0100

    Implement unit WebMvcTests for MainPageControllerTest and RedirectControllerTest.

---
+++ src/test/java/pl/jacekhorabik/urlshortener/mainpage/MainPageControllerTest.java
@@ -0,0 +1,45 @@
+package pl.jacekhorabik.urlshortener.mainpage;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import pl.jacekhorabik.urlshortener.view.ViewName;
+
+@WebMvcTest(controllers = MainPageController.class)
+class MainPageControllerTest {
+
+  @Autowired private MockMvc mockMvc;
+
+  @MockitoBean private UrlShorteningService urlShorteningService;
+
+  @Test
+  void shouldReturnMainPage() throws Exception {
+    this.mockMvc
+        .perform(get("/v1/"))
+        .andExpect(status().isOk())
+        .andExpect(view().name(ViewName.MAIN_PAGE.viewName()))
+        .andExpect(model().attributeExists("requestUrlDTO"));
+  }
+
+  @Test
+  void shouldReturnMainPageWithShortenedUrl() throws Exception {
+    final String url = "http://example.com";
+
+    when(urlShorteningService.shortenUrl(new UrlDTO(url)))
+        .thenReturn(new UrlEntity("hash123", url));
+
+    this.mockMvc
+        .perform(post("/v1/").param("url", url))
+        .andExpect(status().isCreated())
+        .andExpect(view().name(ViewName.MAIN_PAGE.viewName()))
+        .andExpect(model().attribute("requestUrlDTO", new UrlDTO()))
+        .andExpect(
+            model().attribute("responseUrlDTO", new UrlDTO("http://localhost:8080/v1/r/hash123")));
+  }
+}
---
+++ src/test/java/pl/jacekhorabik/urlshortener/mainpage/RedirectControllerTest.java
@@ -0,0 +1,47 @@
+package pl.jacekhorabik.urlshortener.mainpage;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import pl.jacekhorabik.urlshortener.view.ViewName;
+
+@WebMvcTest(controllers = RedirectController.class)
+class RedirectControllerTest {
+
+  @Autowired private MockMvc mockMvc;
+
+  @MockitoBean private UrlShorteningService urlShorteningService;
+
+  @Test
+  void shouldRedirectToCorrectShortenedUrl() throws Exception {
+    final String hash = "hash234";
+    final String url = "http://example.com";
+
+    when(urlShorteningService.findUrlByHash(hash))
+        .thenReturn(Optional.of(new UrlEntity(hash, url)));
+
+    mockMvc
+        .perform(get("/v1/r/" + hash))
+        .andExpect(status().is(HttpStatus.FOUND.value()))
+        .andExpect(view().name("redirect:" + url));
+  }
+
+  @Test
+  void shouldRedirectToCustomNotFoundPage() throws Exception {
+    final String hash = "hash";
+    when(urlShorteningService.findUrlByHash(hash)).thenReturn(Optional.empty());
+
+    mockMvc
+        .perform(get("/v1/r/" + hash))
+        .andExpect(status().is(HttpStatus.NOT_FOUND.value()))
+        .andExpect(view().name(ViewName.NOT_FOUND.viewName()));
+  }
+}
