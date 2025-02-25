753ccac3512e2cb94ffac37a0a028dec876a0e30
Jacek Horabik <jacek.horabik@ext.airbnb.com>
2025-02-25 17:38:55 +0100

    Basic skeleton of creating users with USER role.

--- src/main/java/pl/jacekhorabik/urlshortener/keycloak/KeycloakController.java
+++ src/main/java/pl/jacekhorabik/urlshortener/keycloak/KeycloakController.java
@@ -5,8 +5,11 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.resource.RealmResource;
+import org.keycloak.admin.client.resource.UserResource;
 import org.keycloak.representations.idm.CredentialRepresentation;
+import org.keycloak.representations.idm.RoleRepresentation;
 import org.keycloak.representations.idm.UserRepresentation;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import pl.jacekhorabik.urlshortener.common.security.UserRole;
 import pl.jacekhorabik.urlshortener.keycloak.config.KeycloakProperties;
@@ -14,7 +17,7 @@ import pl.jacekhorabik.urlshortener.keycloak.config.KeycloakProperties;
 @RestController
 @RequestMapping("/v1")
 @RequiredArgsConstructor
-public class KeycloakController {
+class KeycloakController {
 
   private final Keycloak keycloak;
   private final KeycloakProperties keycloakProperties;
@@ -26,7 +29,6 @@ public class KeycloakController {
     UserRepresentation user = new UserRepresentation();
     user.setEmail(username + "@onet.pl");
     user.setUsername(username);
-    user.setRealmRoles(List.of(UserRole.USER.toString()));
     user.setEnabled(true);
     user.setEmailVerified(true);
 
@@ -34,13 +36,18 @@ public class KeycloakController {
     userCredential.setType(CredentialRepresentation.PASSWORD);
     userCredential.setTemporary(false);
     userCredential.setValue(password);
-
     user.setCredentials(List.of(userCredential));
 
     try (Response response = realm.users().create(user)) {
       int status = response.getStatus();
-      System.out.println("after response");
-      return status;
+      if (!HttpStatus.valueOf(status).is2xxSuccessful()) {
+        return status;
+      }
     }
+    UserRepresentation createdUser = realm.users().search((user.getUsername())).get(0);
+    UserResource userResource = realm.users().get(createdUser.getId());
+    RoleRepresentation userRole = realm.roles().get(UserRole.USER.toString()).toRepresentation();
+    userResource.roles().realmLevel().add(List.of(userRole));
+    return 0;
   }
 }
