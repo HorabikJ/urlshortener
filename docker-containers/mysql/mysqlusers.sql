ee047a5159c08d9e95261d63c0b1b94fa2d4b528
Jacek Horabik <jacek.horabik@ext.airbnb.com>
2025-03-03 16:13:01 +0100

    Reformat code.

--- docker-containers/mysql/mysqlusers.sql
+++ docker-containers/mysql/mysqlusers.sql
@@ -8,7 +8,7 @@ DROP USER IF EXISTS `keycloak-admin`@`%`;
 CREATE DATABASE IF NOT EXISTS urlshortener CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE USER IF NOT EXISTS `urlshortener-admin`@`%` IDENTIFIED BY 'password';
 GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, REFERENCES, INDEX, ALTER, EXECUTE, CREATE VIEW, SHOW VIEW,
-CREATE ROUTINE, ALTER ROUTINE, EVENT, TRIGGER ON `urlshortener`.* TO `urlshortener-admin`@`%`;
+    CREATE ROUTINE, ALTER ROUTINE, EVENT, TRIGGER ON `urlshortener`.* TO `urlshortener-admin`@`%`;
 
 CREATE USER IF NOT EXISTS `urlshortener-user`@`%` IDENTIFIED BY 'password';
 GRANT SELECT, INSERT, UPDATE, DELETE, SHOW VIEW ON `urlshortener`.* TO `urlshortener-user`@`%`;
