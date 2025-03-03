defd66ded1ba7188a1f719cfbd49ab8f310bf8a8
Jacek Horabik <jacek.horabik@ext.airbnb.com>
2025-03-03 16:12:11 +0100

    Create mysql db schema and users for separate keycloak schema.

--- docker-containers/mysql/mysqlusers.sql
+++ docker-containers/mysql/mysqlusers.sql
@@ -1,11 +1,21 @@
--- Run this script in MySql Workbench to create database, and users.
 DROP DATABASE IF EXISTS urlshortener;
+DROP DATABASE IF EXISTS keycloak;
+
 DROP USER IF EXISTS `urlshortener-admin`@`%`;
 DROP USER IF EXISTS `urlshortener-user`@`%`;
+DROP USER IF EXISTS `keycloak-admin`@`%`;
+
 CREATE DATABASE IF NOT EXISTS urlshortener CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE USER IF NOT EXISTS `urlshortener-admin`@`%` IDENTIFIED BY 'password';
 GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, REFERENCES, INDEX, ALTER, EXECUTE, CREATE VIEW, SHOW VIEW,
 CREATE ROUTINE, ALTER ROUTINE, EVENT, TRIGGER ON `urlshortener`.* TO `urlshortener-admin`@`%`;
+
 CREATE USER IF NOT EXISTS `urlshortener-user`@`%` IDENTIFIED BY 'password';
 GRANT SELECT, INSERT, UPDATE, DELETE, SHOW VIEW ON `urlshortener`.* TO `urlshortener-user`@`%`;
+
+CREATE DATABASE IF NOT EXISTS keycloak CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE USER IF NOT EXISTS `keycloak-admin`@`%` IDENTIFIED BY 'password';
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, REFERENCES, INDEX, ALTER, EXECUTE, CREATE VIEW, SHOW VIEW,
+    CREATE ROUTINE, ALTER ROUTINE, EVENT, TRIGGER ON `keycloak`.* TO `keycloak-admin`@`%`;
+
 FLUSH PRIVILEGES;
