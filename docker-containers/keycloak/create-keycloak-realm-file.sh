1dea9df76fcefd917b33da35a7c04d844504385d
Jacek Horabik <jacek.horabik@ext.airbnb.com>
2025-04-03 22:05:11 +0200

    Make create-keycloak-realm-file.sh professional.

--- docker-containers/keycloak/create-keycloak-realm-file.sh
+++ docker-containers/keycloak/create-keycloak-realm-file.sh
@@ -1,9 +1,9 @@
 #!/bin/zsh
 
 ./replace-realm-vars.sh \
-http://localhost:30008 \
-http://urlshortener-keycloak-service.urlshortener-dev:8080 \
-http://localhost:30009 \
-qQUimZxrmnop3ug6UqctmCKgkqPVIhV9 \
-yuvncagklxranszg
+--app-base-url "http://localhost:30008" \
+--keycloak-internal-base-url "http://urlshortener-keycloak-service.urlshortener-dev:8080" \
+--keycloak-external-base-url "http://localhost:30009" \
+--urlshortener-client-secret "qQUimZxrmnop3ug6UqctmCKgkqPVIhV9" \
+--gmail-app-password "yuvncagklxranszg"
 exit 0
--- docker-containers/keycloak/replace-realm-vars.sh
+++ docker-containers/keycloak/replace-realm-vars.sh
@@ -1,13 +1,68 @@
 #!/bin/bash
 
-rm ./realm/*.json
-cp ./urlshortener-keycloak-realm-template.json ./realm/urlshortener-keycloak-realm.json
+# Define default values and help function
+function show_usage {
+    echo "Usage: $0 [options]"
+    echo "Options:"
+    echo "  --app-base-url                        Base URL of the application"
+    echo "  --keycloak-internal-base-url          Internal base URL for Keycloak"
+    echo "  --keycloak-external-base-url          External base URL for Keycloak"
+    echo "  --urlshortener-client-secret          Client secret for URL shortener"
+    echo "  --gmail-app-password                  Gmail app password"
+    echo "  --help                                Show this help message"
+    exit 1
+}
 
-appBaseUrl=$1
-keycloakInternalBaseUrl=$2
-keycloakExternalBaseUrl=$3
-urlshortenerClientSecret=$4
-gmailAppPassword=$5
+# Initialize variables
+appBaseUrl=""
+keycloakInternalBaseUrl=""
+keycloakExternalBaseUrl=""
+urlshortenerClientSecret=""
+gmailAppPassword=""
+
+# Parse named parameters
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --app-base-url)
+            appBaseUrl="$2"
+            shift 2
+            ;;
+        --keycloak-internal-base-url)
+            keycloakInternalBaseUrl="$2"
+            shift 2
+            ;;
+        --keycloak-external-base-url)
+            keycloakExternalBaseUrl="$2"
+            shift 2
+            ;;
+        --urlshortener-client-secret)
+            urlshortenerClientSecret="$2"
+            shift 2
+            ;;
+        --gmail-app-password)
+            gmailAppPassword="$2"
+            shift 2
+            ;;
+        --help)
+            show_usage
+            ;;
+        *)
+            echo "Unknown parameter: $1"
+            show_usage
+            ;;
+    esac
+done
+
+# Check if required parameters are provided
+if [[ -z "$appBaseUrl" || -z "$keycloakInternalBaseUrl" || -z "$keycloakExternalBaseUrl" || 
+      -z "$urlshortenerClientSecret" || -z "$gmailAppPassword" ]]; then
+    echo "Error: Missing required parameters"
+    show_usage
+fi
+
+# Copy template file
+rm -f ./realm/*.json
+cp ./urlshortener-keycloak-realm-template.json ./realm/urlshortener-keycloak-realm.json
 
 # this empty '' in below sed commands is applicable only for macOS, remove it in linux
 sed -i '' "s|<APP-BASE-URL>|$appBaseUrl|g" ./realm/urlshortener-keycloak-realm.json
@@ -16,4 +71,5 @@ sed -i '' "s|<KEYCLOAK-EXTERNAL-BASE-URL>|$keycloakExternalBaseUrl|g" ./realm/ur
 sed -i '' "s|<URL-SHORTENER-CLIENT-SECRET>|$urlshortenerClientSecret|g" ./realm/urlshortener-keycloak-realm.json
 sed -i '' "s|<GMAIL-APP-PASSWORD>|$gmailAppPassword|g" ./realm/urlshortener-keycloak-realm.json
 
+echo "Configuration completed successfully"
 exit 0
