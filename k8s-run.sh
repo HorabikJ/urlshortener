f70ff5e53c8f98a25305b4b83e550e775ed2ad53
Jacek Horabik <jacek.horabik@ext.airbnb.com>
2025-04-04 09:59:31 +0200

    Rename file. Delete redundant files.

--- k8s-run.sh
+++ k8s-run.sh
@@ -5,11 +5,11 @@ kubectl delete namespace urlshortener-dev
 docker image rm urlshortener-keycloak-k8s
 docker build -t urlshortener-keycloak-k8s:latest ./docker-containers/keycloak
 
-docker image rm urlshortener-backend
-docker build -t urlshortener-backend:latest .
+#docker image rm urlshortener-backend
+#docker build -t urlshortener-backend:latest .
 
-docker image rm urlshortener-db
-docker build -t urlshortener-db:latest ./docker-containers/mysql/ 
+#docker image rm urlshortener-db
+#docker build -t urlshortener-db:latest ./docker-containers/mysql/ 
 
 kubectl create -f ./k8s/urlshortener-namespace.yaml
 kubectl create -f ./k8s/db/urlshortener-db-deployment.yaml
