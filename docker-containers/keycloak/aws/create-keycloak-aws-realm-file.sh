fa4646e8448c10696628b04685185e34d2437fcb
Jacek Horabik <jacek.horabik@ext.airbnb.com>
2025-11-15 20:42:37 +0100

    Change keycloak internal url.

--- docker-containers/keycloak/aws/create-keycloak-aws-realm-file.sh
+++ docker-containers/keycloak/aws/create-keycloak-aws-realm-file.sh
@@ -5,7 +5,7 @@ mkdir -p realm
 ./../realm-template/replace-realm-vars.sh \
 --app-external-base-url "http://56.228.16.205" \
 --app-internal-base-url "http://172.31.21.177" \
---keycloak-external-base-url "http://13.62.58.185" \
+--keycloak-external-base-url "http://13.61.1.70" \
 --keycloak-internal-base-url "http://172.31.19.242" \
 --urlshortener-client-secret "qQUimZxrmnop3ug6UqctmCKgkqPVIhV9" \
 --gmail-app-password "yuvncagklxranszg"
