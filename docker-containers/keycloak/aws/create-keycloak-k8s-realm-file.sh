89427df25bdf58e3d77026fa654ba17cc13cf6d7
Jacek Horabik <jacek.horabik@ext.airbnb.com>
2025-11-15 19:46:07 +0100

    Set urls in keycloak for aws deployment.

--- docker-containers/keycloak/aws/create-keycloak-k8s-realm-file.sh
+++ docker-containers/keycloak/aws/create-keycloak-k8s-realm-file.sh
@@ -1,10 +1,10 @@
 #!/bin/zsh
 
 ./../realm-template/replace-realm-vars.sh \
---app-external-base-url "http://<configure>:80" \
---app-internal-base-url "http://<configure>:80" \
---keycloak-external-base-url "http://<configure>:80" \
---keycloak-internal-base-url "http://<configure>:80" \
+--app-external-base-url "http://56.228.16.205" \
+--app-internal-base-url "http://172.31.21.177" \
+--keycloak-external-base-url "http://13.62.58.185" \
+--keycloak-internal-base-url "http://172.31.19.242" \
 --urlshortener-client-secret "qQUimZxrmnop3ug6UqctmCKgkqPVIhV9" \
 --gmail-app-password "yuvncagklxranszg"
 exit 0
