530311f2fc31bff720ed9d57f60a92d718d74c2d
Jacek Horabik <jacek.horabik@ext.airbnb.com>
2025-11-15 19:34:58 +0100

    Add keycloak aws Dockerfile.

---
+++ docker-containers/keycloak/aws/create-keycloak-k8s-realm-file.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+./../realm-template/replace-realm-vars.sh \
+--app-external-base-url "http://<configure>:80" \
+--app-internal-base-url "http://<configure>:80" \
+--keycloak-external-base-url "http://<configure>:80" \
+--keycloak-internal-base-url "http://<configure>:80" \
+--urlshortener-client-secret "qQUimZxrmnop3ug6UqctmCKgkqPVIhV9" \
+--gmail-app-password "yuvncagklxranszg"
+exit 0
