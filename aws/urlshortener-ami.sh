5c67c0f6fe1ca44cc985d72ac20019ed807c34f5
Jacek Horabik <jacek.horabik@ext.airbnb.com>
2025-11-16 18:00:43 +0100

    Implement working ami `UserData` Script.

--- aws/urlshortener-ami.sh
+++ aws/urlshortener-ami.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# REMEMBER! This script is run by the `root` user! To make any file changes in `/home/ec2-user` directory you must 
+# run the command with `sudo -u ec2-user` prefix.
+
 # install docker
 sudo dnf update -y
 sudo dnf install docker -y
@@ -9,32 +12,31 @@ sudo systemctl enable docker
 sudo systemctl start docker
 # Add ec2-user to docker group (to run docker without sudo)
 sudo usermod -aG docker ec2-user
-newgrp docker
+
+# install git
+sudo dnf install git -y
+# install java
+sudo dnf install java-21-amazon-corretto-devel -y
+# install maven 
+sudo dnf install maven -y
+# clone repo
+sudo -u ec2-user git clone https://github.com/HorabikJ/urlshortener.git /home/ec2-user/code/urlshortener
 
 # install zsh
 sudo dnf install zsh -y
-#set zsh as default shell
-sudo usermod -s /usr/bin/zsh "$(whoami)"
+# set zsh as default shell for ec2-user user
+sudo sed -i "/^ec2-user:/s|:/bin/bash$|:/usr/bin/zsh|" /etc/passwd
 
 # install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-sed -i 's/^ZSH_THEME=.*$/ZSH_THEME="powerlevel10k\/powerlevel10k"/' ~/.zshrc
-sed -i 's/^plugins=(.*)$/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
+sudo -u ec2-user sh -c 'export RUNZSH=no; export CHSH=no; sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended'
+sudo -u ec2-user git clone https://github.com/romkatv/powerlevel10k.git /home/ec2-user/.oh-my-zsh/custom/themes/powerlevel10k
+sudo -u ec2-user git clone https://github.com/zsh-users/zsh-autosuggestions /home/ec2-user/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+sudo -u ec2-user git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /home/ec2-user/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+sudo -u ec2-user sed -i 's/^ZSH_THEME=.*$/ZSH_THEME="powerlevel10k\/powerlevel10k"/' /home/ec2-user/.zshrc
+sudo -u ec2-user sed -i 's/^plugins=(.*)$/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' /home/ec2-user/.zshrc
 
-# install git
-sudo yum install git -y
-#install sdkman
-curl -s "https://get.sdkman.io" | bash
-source "/home/ec2-user/.sdkman/bin/sdkman-init.sh"
-# install java
-sdk install java 21.0.5-amzn
-# install maven 
-sdk install maven 3.9.9
-# clone repo
-git clone https://github.com/HorabikJ/urlshortener.git ~/code/urlshortener
+# apply all the changes made in /home/ec2-user/.zshrc 
+source /home/ec2-user/.zshrc
 
 # cd ~/code/urlshortener/ || exit
 # mvn dependency:resolve
