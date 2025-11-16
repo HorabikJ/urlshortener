843cb150adbab381a3531785689c0228be2dc118
Jacek Horabik <jacek.horabik@ext.airbnb.com>
2025-11-16 18:36:42 +0100

    Set correct ips in Keycloak container.

--- docker-containers/keycloak/aws/create-keycloak-aws-realm-file.sh
+++ docker-containers/keycloak/aws/create-keycloak-aws-realm-file.sh
@@ -3,10 +3,10 @@
 mkdir -p realm
 
 ./../realm-template/replace-realm-vars.sh \
---app-external-base-url "http://56.228.16.205" \
---app-internal-base-url "http://172.31.21.177" \
---keycloak-external-base-url "http://16.16.65.83" \
---keycloak-internal-base-url "http://172.31.19.242" \
+--app-external-base-url "http://13.51.249.172" \
+--app-internal-base-url "http://172.31.11.141" \
+--keycloak-external-base-url "http://16.16.255.34" \
+--keycloak-internal-base-url "http://172.31.14.104" \
 --urlshortener-client-secret "qQUimZxrmnop3ug6UqctmCKgkqPVIhV9" \
 --gmail-app-password "yuvncagklxranszg"
 exit 0
