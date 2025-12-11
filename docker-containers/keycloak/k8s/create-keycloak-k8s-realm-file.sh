ccaec253de27dc527597532aba96375e1c1bbf79
Jacek Horabik <jacek.horabik@ext.airbnb.com>
2025-12-11 15:07:23 +0100

    Rename docker images.

--- docker-containers/keycloak/k8s/create-keycloak-k8s-realm-file.sh
+++ docker-containers/keycloak/k8s/create-keycloak-k8s-realm-file.sh
@@ -4,7 +4,7 @@ mkdir -p realm
 
 ./../realm-template/replace-realm-vars.sh \
 --app-external-base-url "http://localhost:30008" \
---app-internal-base-url "http://urlshortener-backend-service.urlshortener-dev:8080" \
+--app-internal-base-url "http://urlshortener-app-service.urlshortener-dev:8080" \
 --keycloak-external-base-url "http://localhost:30009" \
 --keycloak-internal-base-url "http://urlshortener-keycloak-service.urlshortener-dev:8080" \
 --urlshortener-client-secret "qQUimZxrmnop3ug6UqctmCKgkqPVIhV9" \
--- k8s-run.sh
+++ k8s-run.sh
@@ -2,22 +2,23 @@
 
 kubectl delete namespace urlshortener-dev
 
-docker image rm urlshortener-keycloak-k8s
-docker build -t urlshortener-keycloak-k8s:latest ./docker-containers/keycloak/k8s/
+docker image rm urlshortener-keycloak:k8s
+docker build -t urlshortener-keycloak:k8s ./docker-containers/keycloak/k8s/
 
-docker image rm urlshortener-backend
-docker build -t urlshortener-backend:latest .
+docker image rm urlshortener-app:k8s
+docker build -t urlshortener-app:k8s .
 
-docker image rm urlshortener-db
-docker build -t urlshortener-db:latest ./docker-containers/mysql/ 
+docker image rm urlshortener-db:k8s
+docker build -t urlshortener-db:k8s ./docker-containers/mysql/ 
 
 kubectl create -f ./k8s/urlshortener-namespace.yaml
+
 kubectl create -f ./k8s/db/urlshortener-db-deployment.yaml
 kubectl create -f ./k8s/db/urlshortener-db-service.yaml
 #todo replace sleep commands with readiness probes
-sleep 3
+sleep 10
 kubectl create -f ./k8s/keycloak/urlshortener-keycloak-deployment.yaml
 kubectl create -f ./k8s/keycloak/urlshortener-keycloak-service.yaml
-sleep 3
-kubectl create -f ./k8s/backend/urlshortener-backend-deployment.yaml
-kubectl create -f ./k8s/backend/urlshortener-backend-service.yaml
+sleep 10
+kubectl create -f ./k8s/app/urlshortener-app-deployment.yaml
+kubectl create -f ./k8s/app/urlshortener-app-service.yaml
